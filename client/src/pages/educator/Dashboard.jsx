ebcd4062315c0e760cd3f5a59377177a0d93dd37
Samay Kothari <samaykothari305@gmail.com>
2026-03-19 12:24:00 +0530

    files order

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -4,7 +4,7 @@ import { AppContext } from '../../context/AppContext';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import Loading from '../../components/student/Loading';
-import DashboardCharts from "./DashboardCharts";
+import DashboardCharts from "../../components/educator/DashboardCharts";
 
 const Dashboard = () => {
 
